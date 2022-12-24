26a85d18d7ec99fe36512e429f51561e5f802486
Martina928 <tinalai101@gmail.com>
2022-12-24 10:15:53 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -79,6 +79,15 @@ app.post('/todos/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// delete
+app.post('/todos/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Todo.findById(id)
+    .then(todo => todo.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // listen and start the server
 app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
